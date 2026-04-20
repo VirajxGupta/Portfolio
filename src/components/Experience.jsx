a30bc61db24af92af4b9c58148df4230f243320d
Viraj Gupta <virajgupta1811@gmail.com>
2026-04-20 17:52:11 +0530

    corrected experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,7 +22,7 @@ const experiences = [
         description: "Planned and executed an annual techno-cultural fest for 1000+ students."
     },
     {
-        role: "development Head",
+        role: "Development Head",
         company: "AARUNYA 2026",
         period: "2026-2026",
         description: "Led a team of 7 and done the whole website development, admin pannel and payment integration and passes generation with unique qr codes with unique ids."
